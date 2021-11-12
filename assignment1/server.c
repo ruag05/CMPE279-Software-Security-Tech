13e13da29ba162c476f3da6204f39400bbc31b92
ruag05 <ruchiragarwal05@gmail.com>
2021-11-11 18:53:24 -0800

    Added code for 'dropping privilege'

--- assignment1/server.c
+++ assignment1/server.c
@@ -1,11 +1,12 @@
 // Server side C/C++ program to demonstrate Socket programming
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
+#include <sys/socket.h> 
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <pwd.h>
 #define PORT 8080
 
 int main(int argc, char const *argv[])
@@ -16,6 +17,7 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
+    char * childUser = "nobody";
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -56,6 +58,20 @@ int main(int argc, char const *argv[])
     pid_t new_pid;
     new_pid = fork();
     if(new_pid == 0){
+
+        printf("\nInside child process with id: %d\n", new_pid);       
+        struct passwd *pw = getpwnam(childUser);
+        if(pw == NULL){
+            printf("\n");
+            return 0;
+        }
+        int val = setuid(pw->pw_uid);
+        if(val == -1){
+            printf("\nError: Privilege was not dropped in the child process");
+            return 0;
+        }
+        printf("\nPrivilege dropped successfully\n");
+       
         valread = read(new_socket, buffer, 1024);
         printf("Read %d bytes: %s\n", valread, buffer);
         send(new_socket, hello, strlen(hello), 0);
